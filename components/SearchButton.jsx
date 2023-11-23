6f82567ce8d918365397f44ce25606435059a128
amine alaoui <omari1amineco@gmail.com>
2023-11-23 16:01:07 +0100

    feat: add some if and else statement in the Searchbutton component

--- components/SearchButton.jsx
+++ components/SearchButton.jsx
@@ -1,7 +1,17 @@
 "use client";
 
+import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import { useFormStatus } from "react-dom";
+
 function SearchButton() {
-  return <button>Search</button>;
+  const { pending } = useFormStatus();
+
+  return (
+    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-4 px-4 rounded-full disabled::opacity-50 disabled:cursor-not-allowed">
+      {pending && "Searching..."}
+      {!pending && <MagnifyingGlassIcon className="h-5 w-5" />}
+    </button>
+  );
 }
 
 export default SearchButton;
