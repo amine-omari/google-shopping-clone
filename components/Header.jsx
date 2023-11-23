9c5cd717a2de9add24ecfa18155dad3a5ddb8a9c
amine alaoui <omari1amineco@gmail.com>
2023-11-23 16:00:14 +0100

    feat: add the google logo to the header and make it a link

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,7 +1,22 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+
 function Header() {
   return (
-    <div>Header</div>
-  )
+    <header>
+      <Link href="/">
+        <Image
+          src="https://links.papareact.com/208"
+          alt="Logo"
+          width={150}
+          height={150}
+          className="object-contain mr-10"
+        />
+      </Link>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
