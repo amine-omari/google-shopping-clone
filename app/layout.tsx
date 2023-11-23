a84c31add25c9e700e93bac1fd1b248441083679
amine alaoui <omari1amineco@gmail.com>
2023-11-23 12:39:16 +0100

    feat: import Header component to layout.tsx file

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Header, {} from "@/components/Header";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -14,6 +15,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
+        <Header />
         <main>{children}</main>
       </body>
     </html>
