df3edcc1ddc118a70467aed6332d137cc93c5f84
amine alaoui <omari1amineco@gmail.com>
2023-11-23 16:03:16 +0100

    feat: import the MagnifyingGlassIcon to Header component

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -15,6 +16,23 @@ function Header() {
           className="object-contain mr-10"
         />
       </Link>
+
+      <div className="w-full md:max-w-2xl">
+        {/* FORM */}
+        <form action="">
+          <div className="flex items-center gap-2 w-full px-4">
+            <div className="flex items-center space-x-2 bg-white shadow-xl rounded-full border-0 px-6 py-4 flex-1">
+              <MagnifyingGlassIcon className="h-5 w-5 text-gray-400" />
+              <input
+                type="text"
+                name="SearchTerm"
+                placeholder="Search..."
+                className="outline-none flex-1"
+              />
+            </div>
+          </div>
+        </form>
+      </div>
     </header>
   );
 }
